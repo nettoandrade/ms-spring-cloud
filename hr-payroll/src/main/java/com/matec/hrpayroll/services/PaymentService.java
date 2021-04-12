b9223d064ce4d690c58bb9fc68e9e35e0aa192c8
Antonio Muniz de Andrade Neto <antonio.andrade@mv.com.br>
2021-04-12 20:50:56 -0300

    FeignClient

--- hr-payroll/src/main/java/com/matec/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/matec/hrpayroll/services/PaymentService.java
@@ -1,30 +1,21 @@
 package com.matec.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.matec.hrpayroll.entities.Payment;
 import com.matec.hrpayroll.entities.Worker;
+import com.matec.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
-	@Autowired
-	private RestTemplate restTemplate;
-
-	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);		
 		
-		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
+	public Payment getPayment(long workerId, int days) {
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
