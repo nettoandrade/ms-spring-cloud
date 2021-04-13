bc0ff725e48c21826af81f307787b05344e022a3
Antonio Muniz de Andrade Neto <antonio.andrade@mv.com.br>
2021-04-13 20:08:06 -0300

    Project hr-eureka-server-created

---
+++ hr-eurecka-server/src/main/java/com/matec/hreurekaserver/HrEureckaServerApplication.java
@@ -0,0 +1,13 @@
+package com.matec.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEureckaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEureckaServerApplication.class, args);
+	}
+
+}
